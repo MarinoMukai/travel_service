58bd227b729c1639967bade8ca437be064829526
naoki267 <90693862+naoki267@users.noreply.github.com>
2024-04-20 17:08:23 +0800

    基礎画面制作

--- app/Http/Controllers/TravelController.php
+++ app/Http/Controllers/TravelController.php
@@ -1,16 +1,82 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use GeminiAPI\Client;
 use GeminiAPI\Resources\Parts\TextPart;
-class TravelController extends Controller {
-    public function index() {
-        
-        return view('Travel.index');
+
+class TravelController extends Controller
+{
+    /**
+     * 自由入力の入力画面を表示する
+     */
+    public function freeinputindex()
+    {
+        return view('Travel.freeinputindex');
     }
 
-    public function store(Request $request) {
+    /**
+     * 自由入力の結果を表示する
+     */
+    public function freeinputstore(Request $request)
+    {
+        // バリデーション
+        $validateMessages = [
+            'api.required' => '入力は必須です。',
+            'api.max' => '100字以内で入力してください。'
+        ];
+
+        $validatedData = $request->validate([
+            'api' => 'required|max:100',
+        ], $validateMessages);
+
+        $requestData = $request['api'];
+        $response = $this->connectGeminiapi($requestData);
+        return back()->with('success', $response);
+    }
+
+    /**
+     * 選択式の入力画面を表示する
+     */
+    public function choiceinputindex()
+    {
+        return view('Travel.choiceinputindex');
+    }
+
+    /**
+     * 選択式の入力結果を表示する
+     */
+    public function choiceinputstore(Request $request)
+    {
+        // バリデーション
+        $validateMessages = [
+            'api.required' => '入力は必須です。',
+            'api.max' => '100字以内で入力してください。'
+        ];
+
+        $validatedData = $request->validate([
+            'api' => 'required|max:100',
+        ], $validateMessages);
+
+        $requestData = $request['api'];
+        $response = $this->connectGeminiapi($requestData);
+        return back()->with('success', $response);
+    }
+
+    /**
+     * チャット入力画面を表示する
+     */
+    public function chatinputindex()
+    {
+        return view('Travel.chatinputindex');
+    }
+
+    /**
+     * チャット入力の結果を表示する
+     */
+    public function chatinputstore(Request $request)
+    {
         // バリデーション
         $validateMessages = [
             'api.required' => '入力は必須です。',
@@ -29,7 +95,8 @@ class TravelController extends Controller {
     /**
      * geminiapiに接続する(restapiで書きたい)
      */
-    private function connectGeminiapi($requestData) {
+    private function connectGeminiapi($requestData)
+    {
         $geminiApiKey = getenv('GEMINI_API_KEY');
         $client = new Client($geminiApiKey);
 
---
+++ resources/views/Top/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>トップ画面</title>
+</head>
+
+<body>
+    <form action="{{ route('freeInput.page') }}" method="GET">
+        <button type="submit">自由記述式</button>
+    </form>
+    <form action="{{ route('choiceInput.page') }}" method="GET">
+        <button type="submit">選択式</button>
+    </form>
+    <form action="{{ route('chatInput.page') }}" method="GET">
+        <button type="submit">チャット式</button>
+    </form>
+</body>
+
+</body>
+
+</html>
